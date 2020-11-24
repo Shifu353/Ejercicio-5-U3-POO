f480d33f38e13ee4a6bde31347865a585df19a19
Shifu353 <68873489+Shifu353@users.noreply.github.com>
2020-11-23 22:16:27 -0300

    Create Ejercicio5.py

---
+++ Ejercicio5.py
@@ -0,0 +1,9 @@
+from zope.interface import interface
+
+class Coleccion (interface):
+    def InsertarElemento (elemento,pocision):
+        pass
+    def AgregarElemento (elemento):
+        pass
+    def MostrarElemento (posicion):
+        pass
